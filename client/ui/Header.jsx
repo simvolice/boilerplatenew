4c765e4df921d70e5682ad8401e220d0ef81baef
Sardorbek <sardor.majano@gmail.com>
2016-07-12 18:13:28 +0600

    starter template added

--- client/ui/Header.jsx
+++ client/ui/Header.jsx
@@ -1,17 +1,11 @@
 import React, {Component} from 'react';
 import Nav from './Nav.jsx';
-import SideNav from './SideNav.jsx';
-
 
 export default class Header extends Component {
-  componentDidMount() {
-    $(".button-collapse").sideNav();
-  }
   render() {
     return (
       <header>
         <Nav />
-        <SideNav />
       </header>
     );
   }
--- client/ui/Nav.jsx
+++ client/ui/Nav.jsx
@@ -9,12 +9,13 @@ export default class Nav extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo button-collapse"><i className="material-icons">menu</i></a>
+          <a href="#" className="brand-logo">Logo</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><a href="sass.html">Sass</a></li>
             <li><a href="badges.html">Components</a></li>
             <li><a href="collapsible.html">JavaScript</a></li>
           </ul>
+          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
         </div>
       </nav>
     );
